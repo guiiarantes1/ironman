6441e6d36c3c993f91333e519ef07b6376f676ee
Guilherme <guii_arantes@hotmail.com>
2022-10-25 17:20:16 -0300

    alteracoes codigos

--- capflix/src/app/questionario/questionario.component.ts
+++ capflix/src/app/questionario/questionario.component.ts
@@ -10,11 +10,11 @@ export class QuestionarioComponent implements OnInit {
 
   quizzes: Quiz[] = [];
 
-  currentQuiz = 0; 
-  answerSelected=false;
+  currentQuiz = 0;
+  answerSelected = false;
   correctAnswers = 0;
   incorrectAnswers = 0;
-  valorSelecionado = "";
+  valorSelecionado = '';
 
 
 
@@ -23,27 +23,34 @@ export class QuestionarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.quizzes = this.quizService.getQuizzes();
+
   }
 
-  onAnswer(option: boolean){
+  onAnswer(option: boolean, valor: string) {
     this.answerSelected = true;
+    this.valorSelecionado = valor;
+
     setTimeout(() => {
       this.currentQuiz++;
       this.answerSelected = false;
-
-  },200);
-
-    if(option){
-      this.correctAnswers++;
-    }else{
-      this.incorrectAnswers++;
-    }
- 
-  }
   
    
-  
 
+
+    }, 200);
+
+    if (option) {
+      this.correctAnswers++;
+      
+    } else {
+      this.incorrectAnswers++
+    
+ 
+    }
+
+
+
+  }
 
 }
 
